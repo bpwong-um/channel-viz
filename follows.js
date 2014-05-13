09021efb89f43dd406040e159d5bbe05e1368b96
bpwong-um <bpwong@umich.edu>
2014-05-13 15:29:37 -0400

    Update follow.js
    Added account info/display info

--- follows.js
+++ follows.js
@@ -12,13 +12,13 @@
 		hideForm		= 0;
 	*/
 
-	var defaultKey		= '', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [], // Comma separated array of Xively Feed ID numbers
-		applicationName	= '', // Replaces Xively logo in the header
-		dataDuration	= '', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
+	var defaultKey		= 'uNrFCXLnAa2XKu4IfatmrFXNpLLpoaA8qNrZAgfnH9M8xSYL', // Unique master Xively API key to be used as a default
+		defaultFeeds	= [1821537225], // Comma separated array of Xively Feed ID numbers
+		applicationName	= 'kLabUM Test', // Replaces Xively logo in the header
+		dataDuration	= '3days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
-		hideForm		= 0; // To hide input form use value of 1, otherwise set to 0
+		dataColor	= 'FFFF00', // CSS HEX value of color to represent data (omit leading #)
+		hideForm	= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
 
