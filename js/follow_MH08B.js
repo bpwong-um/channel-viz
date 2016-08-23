de0c89e102b290e14a3e3c71d7952df4f818d1f9
bpwong <bpwong@berkeley.edu>
2016-08-22 20:54:49 -0400

    Update webpage titles to show site names rather than Kerkez Lab

--- js/follow_MH08B.js
+++ js/follow_MH08B.js
@@ -14,7 +14,7 @@
 
 	var defaultKey		= 'URbAQZni3e4QD8mJU40dXPLCjlh1b2N6iOWbdaM1WmKc4TE6', // Unique master Xively API key to be used as a default
 		defaultFeeds	= [1239681147], // Comma separated array of Xively Feed ID numbers
-		applicationName	= 'Kerkez lab - Storm water nodes', // Replaces Xively logo in the header
+		applicationName	= 'MH08B', // Replaces Xively logo in the header
 		dataDuration	= '3days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
 		dataColor	= '0000FF', // CSS HEX value of color to represent data (omit leading #)
